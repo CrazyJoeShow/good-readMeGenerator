0bf1c5e41e5764275bcaa511084dd2545e5c6417
Joe Lowery <joseph.loweryit@gmail.com>
2020-09-24 22:26:42 -0400

    Readme for the howework written out

---
+++ action.md
@@ -0,0 +1,14 @@
+## Process for Build
+
+Readme generator with node.js via the command line
+
+-Generator runs indexjs is called
+-Will prompt the user to take imputs to build the good Readme
+-Readme contains
+1.Title
+2.Descriptions
+3.Installation instructions
+4.Table of contents
+5.Licensing
+6.Usage
+7.Testing
--- index.js
+++ index.js
@@ -8,15 +8,68 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 // Array of questins for inputs
 const questions = [];
 
-inquirer.prompt([
-	{
-		type: 'input',
-		name: 'title',
-		message: 'What is the title of your project?',
-	},
-	{
-		type: 'input',
-		name: 'description',
-		message: 'Please provide a description of your project',
-	},
-]);
+inquirer
+	.prompt([
+		{
+			type: 'input',
+			name: 'title',
+			message: 'What is the title of your project?',
+		},
+		{
+			type: 'input',
+			name: 'description',
+			message: 'Please provide a description of your project',
+		},
+		{
+			type: 'input',
+			name: 'username',
+			message: 'Please provide your github username',
+		},
+		{
+			type: 'input',
+			name: 'installation',
+			message:
+				'Please provide installation instructions in order separated by commas',
+		},
+		{
+			type: 'input',
+			name: 'usage',
+			message: 'Please provide usage criteria for your project',
+		},
+		{
+			type: 'list',
+			name: 'license',
+			message: 'Please provide the license, if any your project is using',
+			choices: ['MIT', 'GNU', 'APM', 'NPM', 'GitHub', 'CPAN'],
+		},
+		{
+			type: 'input',
+			name: 'contributing',
+			message: 'Please add how others can contribute:',
+		},
+		{
+			type: 'input',
+			name: 'tests',
+			message: 'Please provide how others may test this project:',
+		},
+		{
+			type: 'input',
+			name: 'fullname',
+			message: 'Please provide your full name:',
+		},
+		{
+			type: 'input',
+			name: 'questions',
+			message:
+				'Please provide your email address asssociated with this project:',
+		},
+	])
+	.then((answers) => {
+		var newReadme = generateMarkdown(answers);
+		fs.writeFile('exampleREADME.md', newReadme, (err) => {
+			if (err) throw err;
+			console.log('look at tat README!!!');
+		});
+	});
+function init() {}
+init();
