be39e710a72c49402e5bd7b84d7dd444e97c9a54
Joe Lowery <joseph.loweryit@gmail.com>
2020-09-24 22:10:51 -0400

    adding commit again for object of inputs forgot git add previously

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello ya dingus');
+//console.log('Hello ya dingus');
+// questions for user
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// Array of questins for inputs
+const questions = [];
+
+inquirer.prompt([
+	{
+		type: 'input',
+		name: 'title',
+		message: 'What is the title of your project?',
+	},
+	{
+		type: 'input',
+		name: 'description',
+		message: 'Please provide a description of your project',
+	},
+]);
